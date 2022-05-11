313c7f07d978299bfa6fc37b4f1c72b150d213d4
Sergio Rubio <srubio@heicommmunity.com>
2022-05-11 10:02:40 -0500

    Mapping entities to db context

--- DirectoryApi/Repositories/ApplicationDbContext.cs
+++ DirectoryApi/Repositories/ApplicationDbContext.cs
@@ -13,6 +13,11 @@ public class ApplicationDbContext : DbContext
     public DbSet<Member> Members => Set<Member>();
     public DbSet<Friendship> Friendships => Set<Friendship>();
 
+    // Auth
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UsersProfiles { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
@@ -36,5 +41,30 @@ public class ApplicationDbContext : DbContext
                 x.WithOwner().HasForeignKey();
             })
         );
+
+        modelBuilder.Entity<UserProfile>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+            entity.HasIndex(p => p.ProfileId);
+            entity.HasIndex(p => p.RoleId);
+            entity.HasOne(p => p.Profile).WithMany().HasForeignKey(p => p.ProfileId);
+            entity.HasOne(p => p.Role).WithMany().HasForeignKey(p => p.RoleId);
+            entity.OwnsMany(p => p.RefreshTokens, a =>
+            {
+                a.WithOwner().HasForeignKey("user_id");
+                a.HasKey(x => x.Id);
+            });
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+        });
+
     }
 }
